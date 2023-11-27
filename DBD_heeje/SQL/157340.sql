873bacc0f941bca20da4be6637fab4454843a205
HeeJeYang <ghjadmb21@gmail.com>
2023-11-27 22:26:58 +0900

    157340번 문제 해결

---
+++ DBD_heeje/SQL/157340.sql
@@ -0,0 +1,15 @@
+/* [Programmers] 157340. 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기 */
+
+SELECT CAR_ID,
+    CASE
+        WHEN CAR_ID IN (
+            SELECT CAR_ID
+            FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+            WHERE START_DATE <= '2022-10-16' AND END_DATE >= '2022-10-16'
+        )
+        THEN "대여중"
+        ELSE "대여 가능"
+    END as AVAILABILITY
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+ORDER BY CAR_ID desc;
